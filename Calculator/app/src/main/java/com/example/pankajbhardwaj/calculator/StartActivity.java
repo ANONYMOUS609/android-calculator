c4be2ab2c8d5b92a678049702d326b30048b7b6a
hsaaitlan <pankajbhardwaj1798@gmail.com>
2019-06-07 12:59:28 +0530

    Second Commit

--- Calculator/app/src/main/java/com/example/pankajbhardwaj/calculator/StartActivity.java
+++ Calculator/app/src/main/java/com/example/pankajbhardwaj/calculator/StartActivity.java
@@ -2,11 +2,29 @@ package com.example.pankajbhardwaj.calculator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button btn = (Button) findViewById(R.id.btn1);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        final int widht = dm.widthPixels;
+        int height = dm.heightPixels;
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = (TextView) findViewById(R.id.txtv);
+                String str = "125+25";
+                int ans = Integer.parseInt(str);
+                tv.setText(str+"1");
+            }
+        });
     }
 }
